ee93b7a8ce7cc92d7f08907aba6aaa8e78850acc
takeshiyoshida76-cell <takeshiyoshida76@gmail.com>
2025-09-07 01:11:56 +0900

    Update write_time.c

--- src/multiLanguageCodes/write_time.c
+++ src/multiLanguageCodes/write_time.c
@@ -1,34 +1,60 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main() {
-    FILE *outfile;
+#define FILENAME "MYFILE.txt"
+#define TIMESTAMP_BUFFER_SIZE 20
+
+/**
+ * @brief Appends a timestamped log message to a file.
+ *
+ * @param filename The path to the file.
+ * @return Returns true on success, false on failure.
+ */
+bool writeTime(const char* filename) {
+    FILE* outfile = NULL;
     time_t t;
-    struct tm *tm_info;
-    char nowtime[20]; // YYYY/MM/DD HH:MM:SS
+    struct tm* tm_info;
+    char nowtime[TIMESTAMP_BUFFER_SIZE];
 
-    // Open Outfile
-    outfile = fopen("MYFILE.txt", "a");
+    // Open file in append mode.
+    outfile = fopen(filename, "a");
     if (outfile == NULL) {
         perror("Failed to open file");
-        return 1;
+        return false;
     }
 
-    // Write message
-    fprintf(outfile, "This program is written in C.\n");
-
-    // Get system datetime
+    // Get the current system date and time.
     t = time(NULL);
     tm_info = localtime(&t);
+    if (tm_info == NULL) {
+        fprintf(stderr, "Failed to get local time.\n");
+        fclose(outfile);
+        return false;
+    }
 
-    // Edit message
-    strftime(nowtime, sizeof(nowtime), "%Y/%m/%d %H:%M:%S", tm_info);
+    // Format the timestamp string.
+    strftime(nowtime, TIMESTAMP_BUFFER_SIZE, "%Y/%m/%d %H:%M:%S", tm_info);
 
-    // Write message
-    fprintf(outfile, "Current Time = %s\n", nowtime);
+    // Write the messages to the file.
+    fprintf(outfile, "This program is written in C.\n");
+    fprintf(outfile, "Current Time = %s\n\n", nowtime);
 
-    // Close Outfile
+    // Close the file.
     fclose(outfile);
+    return true;
+}
+
+int main() {
+    // Attempt to write the log and check the return value.
+    if (writeTime(FILENAME)) {
+        printf("Successfully appended log to %s.\n", FILENAME);
+    } else {
+        // Error message is already printed inside writeLog.
+        printf("Program terminated with an error.\n");
+        return 1;
+    }
 
     return 0;
 }
