546bcc27addc041a91f32ec8098815582003a82a
takeshiyoshida76-cell <takeshiyoshida76@gmail.com>
2025-09-07 01:31:31 +0900

    Update write_time.sh

--- src/multiLanguageCodes/write_time.sh
+++ src/multiLanguageCodes/write_time.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Define the filename for the log.
 FILENAME="MYFILE.txt"
 
-# Get System Datetime
+# Get the current system date and time in the specified format.
+# The `date` command with the `+` option formats the output.
 NOWTIME=$(date +"%Y/%m/%d %H:%M:%S")
 
-# Write Message
+# Append the first message to the log file.
+# The `>>` operator appends the output to the end of the file, creating it if it doesn't exist.
 echo "This program is written in ShellScript." >> "$FILENAME"
+
+# Append the current timestamp to the log file.
+# The variable `$NOWTIME` is expanded to its value.
 echo "Current Time = $NOWTIME" >> "$FILENAME"
+
+# Display a confirmation message to the user.
+echo "Log appended to '$FILENAME'."
